c9f7d1756a28e488f8f51456bdffd7089b9d4291
Jimmy Byrne <jbyrne5094@gmail.com>
2021-07-21 17:13:57 -0700

    added text over drone images

--- javascript/drones.js
+++ javascript/drones.js
@@ -1,3 +1,10 @@
-const handleImageClick = () => {
-    console.log('the image has been clicked')
+// opens the image in the gallery corresponding to the image caption entered
+const handleThumbDescClick = (clickedImageCaption) => {
+    let galleryIndexToOpen = 1
+    fsLightboxInstances.gallery.props.customAttributes.forEach((imageCaption, index) => {
+        if (imageCaption.caption == clickedImageCaption) {
+            galleryIndexToOpen = index
+        }
+    })
+    fsLightboxInstances['gallery'].open(galleryIndexToOpen);
 }
